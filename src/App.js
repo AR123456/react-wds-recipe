68051fb1feec1e5486fa00475060afc21e48a31b
AR123456 <coloradoz99@gmail.com>
2021-12-13 05:08:59 -0700

    getting started with recipe app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import React, { useState } from "react";
-import Counter from "./Counter";
-import CounterFunctionHook from "./CounterFunctionHook";
-// context has a provider- passes out a value and consumer - consumes the value
-export const ThemeContext = React.createContext();
+import React from "react";
 
 function App() {
-  const [theme, setTheme] = useState("red");
-  return (
-    // wrap return in the proivider here the value is state in application
-    <ThemeContext.Provider value={{ backgroundColor: theme }}>
-      Counter Class Based
-      <Counter initialCount={0}></Counter>
-      Counter Hooks Functional
-      <CounterFunctionHook initialCount={0}></CounterFunctionHook>
-      <button
-        onClick={() =>
-          setTheme((prevTheme) => {
-            return prevTheme === "red" ? "blue" : "red";
-          })
-        }
-      >
-        Set theme
-      </button>
-    </ThemeContext.Provider>
-  );
+  return null;
 }
 
 export default App;
