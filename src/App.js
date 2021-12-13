ed1f7e8bfc0f7d1b6c4b8121234c5ca92cdc2f82
AR123456 <coloradoz99@gmail.com>
2021-12-12 18:21:16 -0700

    counter returning some jsx

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import React, { useState } from "react";
-import Counter from "./Counter";
-import CounterFunctionHook from "./CounterFunctionHook";
-// context has a provider- passes out a value and consumer - consumes the value
-export const ThemeContext = React.createContext();
-
 function App() {
-  const [theme, setTheme] = useState("green");
   return (
-    // wrap return in the proivider here the value is state in application
-    <ThemeContext.Provider value={{ backgroundColor: theme }}>
-      Counter Class Based
-      <Counter initialCount={0}></Counter>
-      Counter Hooks Functional
-      <CounterFunctionHook initialCount={0}></CounterFunctionHook>
-      <button
-        onClick={() =>
-          setTheme((prevTheme) => {
-            return prevTheme === "red" ? "blue" : "red";
-          })
-        }
-      >
-        Set theme
-      </button>
-    </ThemeContext.Provider>
+    <div>
+      <button>+</button>
+      <span>0</span>
+      <button>-</button>
+    </div>
   );
 }
 
